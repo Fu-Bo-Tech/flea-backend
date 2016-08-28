68d8ebda85a9888e8db62054182fa414a3815e1f
黃詩婷 <shih_25@yahoo.com.tw>
2016-08-28 21:42:24 +0800

    add product

---
+++ app/models/product.rb
@@ -0,0 +1,5 @@
+class Product < ApplicationRecord
+	
+
+	belongs_to :owner, class_name: "User", foreign_key: :user_id
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-	root "testpage#index"
+	root "products#index"
   devise_for :users
+
+  namespace :account do
+  	resources :products
+  	resources :testpage
+  end
+
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
