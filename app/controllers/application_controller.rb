472951e70c25791b5b55dd35a2a7944dfc8db500
orga <orga.shih@gmail.com>
2016-09-18 21:38:00 +0800

    add token auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
+  include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery except: :create, with: :exception
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,14 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'
+      resource '*',
+               headers: :any,
+               expose: %w(access-token expiry token-type uid client),
+               methods: [:get, :post, :options, :delete, :put]
+    end
+  end
 end
