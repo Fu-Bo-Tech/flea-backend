f8a934c12f7d56fc8f91a91b17686aa1df911a11
orga <orga.shih@gmail.com>
2016-10-10 10:53:59 +0800

    add create bidding lock of good

--- app/graphql/bidding/add_bidding_mutation.rb
+++ app/graphql/bidding/add_bidding_mutation.rb
@@ -9,9 +9,11 @@ AddBiddingMutation = GraphQL::Relay::Mutation.define do
   resolve Auth.authenticate_user! -> (inputs, ctx) do
     user = ctx[:current_user]
     good = NodeIdentification.object_from_id(inputs[:id], ctx)
-    bidding = good.biddings.build(amount: inputs[:amount], trash_word: inputs[:trashWord])
-    bidding.user = user
-    bidding.save!
+    good.with_lock do
+      bidding = good.biddings.build(amount: inputs[:amount], trash_word: inputs[:trashWord])
+      bidding.user = user
+      bidding.save!
+    end
 
     { bidding: bidding }
   end
