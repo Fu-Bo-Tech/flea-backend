c51b2e53c783f2b39d0c34cf17260f89f0d22189
orga <orga.shih@gmail.com>
2016-10-17 19:48:07 +0800

    change extend time rule to 333111...

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,5 +1,5 @@
 class Good < ApplicationRecord
-  EXTEND_TIME_SERIES = [10, 5, 5, 3, 3, 3].freeze
+  EXTEND_TIME_SERIES = [3, 3, 3].freeze
   EXTEND_TIME_MIN = 1
   belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
   belongs_to :event
